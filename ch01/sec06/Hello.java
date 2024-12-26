e2b40fc8cb96c7a45ce773524480db09943f2635
a06246 <a06246@gmail.com>
2024-12-26 20:50:59 +0900

    ch01 clear

---
+++ ch01/sec06/Hello.java
@@ -0,0 +1,15 @@
+package ch01.sec06;
+/**
+ * 김은아
+ * 1장 : 자바 시작하기
+ * 2024.12.26 ()
+ */
+
+public class Hello {
+	//프로그램 실행 진입
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행
+		System.out.println("Hello, Java");
+	}
+
+}
